74e9e04c69bb569cb603d847e680887779252521
Jolly Demo <itsmeanmolsaxena10@gmail.com>
2026-05-01 15:02:25 +0530

    first lecture

---
+++ L1_1.py
@@ -0,0 +1,15 @@
+#to add comment, use # before the comment
+
+#print statement to print Hello World
+print("Hello World")
+
+a = 3
+print(a)
+
+str = "Hello World - update"
+print(str)
+
+b, c, d  = 1, 2.5, "one line assignment"
+print(b)
+print(c)
+print(d)
